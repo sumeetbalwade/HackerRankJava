175d084e3a5652904fc7c8cf570b183cca1214e3
Sumeet Balwade <balwadesr@gmail.com>
2022-02-15 08:05:14 +0530

    fourth commit

--- javadatatype.java
+++ javadatatype.java
@@ -1,58 +1,82 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
-public class Solution {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String testCases = br.readLine();
-        int N = Integer.parseInt(testCases);
-        for (int i = 0; i < N; i++) {
-            //BufferedReader brr = new BufferedReader(new InputStreamReader(System.in));
-            String line = br.readLine();
-            try {
-                long n = Long.parseLong(line);
-                if (n == 0 || n == 1) {
-                System.out.println(n + " can be fitted in:");
-                System.out.println("* boolean");
-                System.out.println("* byte");
-                System.out.println("* short");
-                System.out.println("* int");
-                System.out.println("* long");
-                continue;
-            } else if (n == (byte)n) {
-                System.out.println(n + " can be fitted in:");
-                System.out.println("* byte");
-                System.out.println("* short");
-                System.out.println("* int");
-                System.out.println("* long");
-                continue;
+import java.io.*;
+
+
+
+
+
+class Solution{
+
+
+    public static void main(String []argh)
+
+
+    {
+
+
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int t=sc.nextInt();
+
+
+
+        for(int i=0;i<t;i++)
+
+
+        {
+
+
+
+            try
+
+
+            {
+
+
+                long x=sc.nextLong();
+
+
+                System.out.println(x+" can be fitted in:");
+
+
+                if(x>=-128 && x<=127)System.out.println("* byte");
+
+
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+
+
+                if(x>=-2147483648 && x<= 2147483647)System.out.println("* int");
+
+
+                if(x>=-9223372036854775808L && x<= 9223372036854775807L)System.out.println("* long");
+
+
             }
-              else if (n == (short)n) {
-                System.out.println(n + " can be fitted in:");
-                System.out.println("* short");
-                System.out.println("* int");
-                System.out.println("* long");
-                continue;
-            } else if (n == (int)n) {
-                System.out.println(n + " can be fitted in:");
-                System.out.println("* int");
-                System.out.println("* long");
-                continue;
-            } else {
-                System.out.println(n + " can be fitted in:");
-                System.out.println("* long");
-                continue;
-           }
-            } catch (NumberFormatException e)  {
-                System.out.println(line + " can't be fitted anywhere.");
-                continue;
+
+
+            catch(Exception e)
+
+
+            {
+
+
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+
 
             }
+
+
+
         }
+
+
     }
+
+
 }
-      
