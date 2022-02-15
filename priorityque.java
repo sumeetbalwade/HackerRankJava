04e9d95352b6a40d2fe838439f259dcb73573791
Sumeet Balwade <balwadesr@gmail.com>
2022-02-15 13:25:49 +0530

    priority queue

--- priorityque.java
+++ priorityque.java
@@ -1,53 +1,265 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
 
-class Student {
-    private final int id;
-    private final String name;
-    private final double cgpa;
-    
-    public Student(int id, String name, double cgpa) {
-        this.id = id;
+import java.util.ArrayList;
+
+
+import java.util.List;
+
+
+import java.util.Scanner;
+
+
+import java.util.*;
+
+
+class Student implements Comparable<Student>{
+
+
+    String name = new String();
+
+
+    double cgpa;
+
+
+    int id;
+
+
+    public Student(String name,double cgpa,int id)
+
+
+    {
+
+
         this.name = name;
+
+
         this.cgpa = cgpa;
+
+
+        this.id = id;
+
+
     }
-    
-    public int getID() {
-        return id;
+
+
+    public String getName(){
+
+
+        return this.name;
+
+
     }
-    
-    public String getName() {
-        return name;
+
+
+    public int compareTo(Student s)
+
+
+    {
+
+
+        if(cgpa == s.cgpa)
+
+
+        {
+
+
+            if(name.compareTo(s.name) == 0)
+
+
+            {
+
+
+                if(id == s.id)
+
+
+                    return 0;
+
+
+                else if (id > s.id)
+
+
+                    return 1;
+
+
+                else
+
+
+                    return -1;
+
+
+            }
+
+
+            else
+
+
+                return name.compareTo(s.name);
+
+
+        }
+
+
+        else if(cgpa > s.cgpa)
+
+
+            return -1;
+
+
+        else
+
+
+            return 1;
+
+
     }
-    
-    public double getCGPA() {
-        return cgpa;
-    }
+
+
 }
 
-class Priorities {
-    private final PriorityQueue<Student> queue = new PriorityQueue<>(
-            Comparator.comparing(Student::getCGPA).reversed()
-                    .thenComparing(Student::getName)
-                    .thenComparing(Student::getID));
-    
-    public List<Student> getStudents(List<String> events) {
-        events.forEach((event) -> {
-            if (event.equals("SERVED")) {
-                queue.poll();
-            } else {
-                String[] details = event.split(" ");
-                
-                queue.add(new Student(Integer.parseInt(details[3]), details[1], Double.parseDouble(details[2])));
+
+
+class Priorities{
+
+
+    public ArrayList<Student> getStudents(List<String> events)
+
+
+    {
+
+
+        int n = events.size();
+
+
+        PriorityQueue<Student> pq = new PriorityQueue<Student>();
+
+
+        for(String i:events)
+
+
+        {
+
+
+            String[] s = new String[4];
+
+
+            s = i.split("\\s");
+
+
+            if(s.length>1)
+
+
+            {
+
+
+                pq.add(new Student(s[1],Double.valueOf(s[2]),Integer.valueOf(s[3])));
+
+
             }
-        });
-        
-        List<Student> students = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            students.add(queue.poll());
+
+
+            else
+
+
+            {
+
+
+                pq.poll();
+
+
+            }
+
+
         }
-        
-        return students;
+
+
+        while(pq.size()>1)
+
+
+        {
+
+
+            System.out.println(pq.poll().name);
+
+
+        }
+
+
+        return new ArrayList<Student>(pq);
+
+
     }
+
+
 }
-     
+
+
+
+public class Solution {
+
+
+    private final static Scanner scan = new Scanner(System.in);
+
+
+    private final static Priorities priorities = new Priorities();
+
+
+    
+
+
+    public static void main(String[] args) {
+
+
+        int totalEvents = Integer.parseInt(scan.nextLine());    
+
+
+        List<String> events = new ArrayList<>();
+
+
+        
+
+
+        while (totalEvents-- != 0) {
+
+
+            String event = scan.nextLine();
+
+
+            events.add(event);
+
+
+        }
+
+
+        
+
+
+        List<Student> students = priorities.getStudents(events);
+
+
+        
+
+
+        if (students.isEmpty()) {
+
+
+            System.out.println("EMPTY");
+
+
+        } else {
+
+
+            for (Student st: students) {
+
+
+                System.out.println(st.getName());
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+}
